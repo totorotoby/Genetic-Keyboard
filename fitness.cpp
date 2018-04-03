d9883295447f3e55895c829acd04cc9d30c44979
Toby Harvey <tharvey@oberlin.edu>
2018-04-03 16:14:30 -0400

    the fitness function

---
+++ fitness.cpp
@@ -0,0 +1,11 @@
+/* fitness.rs
+ Toby Harvey, Dorian Levine, Marouane Abra, Steven Abreu
+
+The fitness function which will be used to evaluate the fitness
+of a given keyboard*/
+
+
+int distance(){
+
+	
+}
