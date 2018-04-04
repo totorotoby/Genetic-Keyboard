7588a063458388ce87018d0bfdca6f1a032ff79e
Toby Harvey <tharvey@oberlin.edu>
2018-04-04 15:20:58 -0400

    added distances to each key

--- keyboard.cpp
+++ keyboard.cpp
@@ -11,7 +11,7 @@ class key{
     public:
     char charcter;
     int distance;
-    //...more
+    //...more maybe
 
 };
 
@@ -20,28 +20,41 @@ class keyboard {
     // Access specifier
     public:
     //keyboard array 
-    key board[32];           
+    key board[32]; 
+    double fitness;
     
+    //constructor
     keyboard(){
-
+        //array to be shuffled
         char to_shuffle[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
         'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v', 
         'w', 'x', 'y', 'z',' ',':','"','<','>','?'};
         std::random_shuffle(&to_shuffle[0],&to_shuffle[31]);
 
+        //adding charcters  and distances to board
         for (int i = 0; i<32 ; i++){
             board[i].charcter = to_shuffle[i];
+            if (i <= 3 || (i >= 6 && i<= 9))
+                board[i].distance = 92;
+            if (i == 4 || i == 10)
+                board[i].distance = 100;
+            if (i == 5)
+                board[i].distance = 150;
+            if ((i >= 11 && i <= 14) || (i >= 17 && i<= 20))
+                board[i].distance = 0;
+            if ((i >= 15 && i <= 16) || (i == 21))
+                board[i].distance = 90;
+            if ((i >= 22 && i <= 25) || (i >= 27 && i<= 31))
+                board[i].distance = 97;               
+            if (i == 26)
+                board[i].distance = 155;
         }
     }
 };
 
-
+//just testing to see if actually worked
 int main(){
 
     keyboard *first_keyboard = new keyboard();
-
-    for (int i = 0; i<32 ; i++){
-            printf("charc is: %c\n",first_keyboard->board[i].charcter);
-        }
 
 }
