4cffa49105d1090bfdac4ee6ae0b2ba2747ec726
Toby Harvey <tharvey@oberlin.edu>
2018-04-04 11:57:20 -0400

    created key class, and made keyboard class have memeber board

--- keyboard.cpp
+++ keyboard.cpp
@@ -1,20 +1,47 @@
 #include <iostream>
-#include <cstdlib>   // for srand and rand
-#include <array>
+#include <cstdlib>   
+#include <algorithm>
+#include <stdio.h>
+#include <random>
 
-class keyboard 
-{
-	// Access specifier
-	public:
 
-	// Data 
-	int keyboard_array[]={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-                    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                    's', 't', 'u', 'v', 'w', 'x', 'y', 'z',' ',':','"','<','>','?'};
-    int size_array=32;               
-    // Member Functions()
-    int  shuffle_array(int array[])
-    {
-    	random_shuffle(&array[0],&array[31])
+// Key class one for each position on the keyboard
+class key{
+
+    public:
+    char charcter;
+    int distance;
+    //...more
+
+};
+
+//Keyboard class fo
+class keyboard {
+    // Access specifier
+    public:
+    //keyboard array 
+    key board[32];           
+    
+    keyboard(){
+
+        char to_shuffle[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+        'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v', 
+        'w', 'x', 'y', 'z',' ',':','"','<','>','?'};
+        std::random_shuffle(&to_shuffle[0],&to_shuffle[31]);
+
+        for (int i = 0; i<32 ; i++){
+            board[i].charcter = to_shuffle[i];
+        }
     }
+};
+
+
+int main(){
+
+    keyboard *first_keyboard = new keyboard();
+
+    for (int i = 0; i<32 ; i++){
+            printf("charc is: %c\n",first_keyboard->board[i].charcter);
+        }
+
 }
