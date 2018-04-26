fb16997960fa5a6931592fee2216a73b6465b998
Toby Harvey <tharvey@oberlin.edu>
2018-04-26 15:08:29 -0400

    started working through algorithim

--- fitness.cpp
+++ fitness.cpp
@@ -9,6 +9,9 @@ and functions that deal with the text we will be using*/
 using namespace std;
 
 
+int simple_index[18] = {0,1,2,11,12,13,22,23,24,7,8,9,18,19,20,29,30,31};
+
+
 string readText(string filename){
 
 	std::ifstream ifs(filename);
@@ -18,11 +21,40 @@ string readText(string filename){
 
 }
 
+int getFitness(string text, keyboard instance){
+
+	int fitness = 0;
+	for (int i = 0; i < content.length(); i++){
+		int keyindex = getLocation(text[i], instance);
+		key *keyloc = keyboard.board[i];
+
+		if (inSimple(i))
+			 
+	}
+}
+
+int getLocation(char charcter, keyboard instance){
+	key *keyloc
+
+	for (int i = 0; i <= 31; i++){
+		if (keyboard.board[i] == charcter)
+			return i;
+	}
+}
+
+int insimple(int i){
+
+	for (int j =  0; j <= 18; j++){
+		if (i == simple_index[j])
+			return 1;
+	}
+}
+
+
 int main(){
 
 	string filename = "testfile";
-	string content = readText(filename);
+	string text = readText(filename);
 	cout << content << endl;
-
 
 }
